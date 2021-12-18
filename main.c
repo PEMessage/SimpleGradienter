adca6e1cfc27c73d6ec1337c0624ba1f038a8a80
Zhuo Jiawei <1165739182@qq.com>
2021-12-19 01:28:12 +0800

    Add Digtal Tube Display Number

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <STC89C5xRC.H>
 #include "mpu6050.h"
-
+#include "digtube.h"
 //*********************************************************
 //主程序
 //*********************************************************
 void main()
 {
+	unsigned int i = 1234;
 	delay(500);		//上电延时
-//	InitLcd();		//液晶初始化
+
 	InitMPU6050();	//初始化MPU6050
 	delay(150);
+	DigTube_LoadNum(i);
 	while(1)
 	{
-		GetData(ACCEL_XOUT_H);
 
-		delay(500 );
+		DigTube_Display();
 	}
 }
